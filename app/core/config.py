06497d91c57ae6cd7c42f4e011e78569396c9423
Sergey Khoetskiy <khoetskiy@gmail.com>
2025-12-11 15:46:41 +0100

    - Add base settings for fastapi - Add settings for db

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,4 @@
+from pydantic import EmailStr
 from pydantic_settings import BaseSettings
 
 
@@ -6,6 +7,27 @@ class Settings(BaseSettings):
 
     app_title: str = 'Система бронирования мест в кафе'
     app_description: str = 'Сервис для бронирования мест в кафе'
+    first_superuser_email: EmailStr | None = None
+    first_superuser_password: str | None = None
+
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    postgres_server: str
+    postgres_port: str
+
+    @property
+    def database_url(self) -> str:
+        """Формирует URL для подключения к БД."""
+        return (
+            f'postgresql+asyncpg://{self.postgres_user}:'
+            f'{self.postgres_password}@{self.postgres_server}:'
+            f'{self.postgres_port}/{self.postgres_db}'
+        )
+
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
 
 
 settings = Settings()
---
+++ app/core/db.py
@@ -0,0 +1,38 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    declared_attr,
+    mapped_column,
+)
+
+from app.core.config import settings
+
+
+class Base(DeclarativeBase):
+    """Базовый класс с автоматическим определением имени таблицы и поля id."""
+
+    @declared_attr
+    def __tablename__(cls) -> str:  # noqa: N805
+        return cls.__name__.lower()
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+
+engine = create_async_engine(settings.database_url)
+
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
+
+
+async def get_async_session() -> AsyncSession:
+    """Асинхронный генератор сессии SQLAlchemy."""
+    async with AsyncSessionLocal() as async_session:
+        yield async_session
