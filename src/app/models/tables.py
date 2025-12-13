2e35ebbf82e59d0f7a0c7276e07d702727aadb50
Marat Garifulun <admin@admin.com>
2025-12-14 02:03:09 +0700

    I001

--- src/app/models/tables.py
+++ src/app/models/tables.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from core.constants import MAX_SEATS_NUMBER, MIN_SEATS_NUMBER
 from core.db import Base
 from models.base import AuditMixin
+from models.cafe import Cafe
 
 
 class Table(Base, AuditMixin):
