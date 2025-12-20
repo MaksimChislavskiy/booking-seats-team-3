edc506bdfe2c0565750e786f7497d35e8f8bb7f6
Sergey Khoetskiy <115252269+Khoetskiy@users.noreply.github.com>
2025-12-20 23:12:36 +0100

    Fix/tableinfo-schema (#102)
    * Rename tables in table
    * Add missing table and media schemas to __init__.py
    * Fix field name from 'seat_number' to 'seats_count' in Table schemas

--- src/app/schemas/__init__.py
+++ src/app/schemas/__init__.py
@@ -1,5 +1,20 @@
 from .cafe import CafeShortInfo
+from .media import MediaData, MediaInfo
+from .table import (
+    TableBase,
+    TableCreate,
+    TableInfo,
+    TableShortInfo,
+    TableUpdate,
+)
 
 __all__ = [
     'CafeShortInfo',
+    'MediaData',
+    'MediaInfo',
+    'TableBase',
+    'TableCreate',
+    'TableInfo',
+    'TableShortInfo',
+    'TableUpdate',
 ]
