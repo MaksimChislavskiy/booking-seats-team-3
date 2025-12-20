39727c5c6a2f13ad3b2e4a77d75986af4c39ac99
magmaster-201124 <magmasters@yandex.ru>
2025-12-21 02:43:09 +0700

    add shemas_table (#40) (#50)
    Add schemas for Table model
    
    ---------
    
    Co-authored-by: Marat Garifulun <admin@admin.com>
    Co-authored-by: Sergey Khoetskiy <khoetskiy@gmail.com>

--- src/app/core/constants.py
+++ src/app/core/constants.py
@@ -6,6 +6,7 @@ INFRA_DIR = BASE_DIR / 'infra'
 # --- Docker ---
 DEFAULT_LOCAL_HOST = 'localhost'
 ENV_RUN_IN_DOCKER = 'RUN_IN_DOCKER'
+TRUE_VALUE = 'yes'
 
 # --- Логирование ---
 LOGS_DIR = Path('logs')
---
+++ src/app/schemas/__init__.py
@@ -0,0 +1,5 @@
+from .cafe import CafeShortInfo
+
+__all__ = [
+    'CafeShortInfo',
+]
