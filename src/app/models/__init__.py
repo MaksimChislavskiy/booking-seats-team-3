e6059475eaeb25f2d6c53b0547975f8f38a0ca9d
unknown <pahomdze@inbox.ru>
2025-12-21 18:37:37 +0700

    added dmodels for import

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,15 +1,19 @@
 from .base import AuditMixin
-from .booking import Booking
+from .booking import Booking, TableSlot
 from .cafe import Cafe
 from .slot import Slot
 from .table import Table
 from .user import User
+from .enum import BookingStatus
+
 
 __all__ = [
     'AuditMixin',
     'Booking',
+    'BookingStatus',
     'Cafe',
     'Slot',
     'Table',
+    'TableSlot',
     'User',
 ]
