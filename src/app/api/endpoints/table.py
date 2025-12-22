53d2d399dddd2414ea6c52d7f18d6ef4a7273bde
Marat Garifulun <admin@admin.com>
2025-12-23 02:49:36 +0700

    draft_2

--- src/app/api/endpoints/table.py
+++ src/app/api/endpoints/table.py
@@ -4,9 +4,9 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.db import get_async_session
-from app.core.user import current_user
+from app.core.user import current_manager_or_admin, current_user
 from crud.table import table_crud
-from models import Table
+from models import Table, User
 from schemas.table import TableInfo
 from validators.table import check_cafe_exists
 
@@ -23,12 +23,16 @@ router = APIRouter()
 async def get_table(
     cafe_id: int,
     table_id: int,
+    current_user: User = Depends(current_user),
     session: AsyncSession = Depends(get_async_session),
 ) -> Table:
     """Получаем стол по ID с учётом прав пользователя."""
     cafe = await check_cafe_exists(cafe_id, session)
-    table = await table_crud.get_by_id(
-        session=session, cafe_id=cafe.id, id=table_id,
+    table = await table_crud.get_by_id_with_permissions(
+        session=session,
+        cafe_id=cafe.id,
+        id=table_id,
+        user_role=current_user.role
     )
     if not table:
         raise HTTPException(
--- src/app/crud/table.py
+++ src/app/crud/table.py
@@ -1,9 +1,36 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.crud.base import CRUDBase
-from app.models import Table
+from app.models import Table, UserRole
 
 
 class CRUDTable(CRUDBase):
     """CRUD для столов."""
+
+    async def get_by_id_with_permissions(
+        self,
+        session: AsyncSession,
+        cafe_id: int,
+        id: int,
+        user_role: UserRole
+    ) -> Table | None:
+        """
+        Получение стола по ID с учётом прав пользователя:
+
+        - Для USER: только активные столы (is_active=True)
+        - Для ADMIN/MANAGER: любые столы
+        """
+        query = select(Table).where(
+            Table.cafe_id == cafe_id,
+            Table.id == id
+        )
+
+        if user_role == UserRole.USER:
+            query = query.where(Table.is_active)
+
+        result = await session.execute(query)
+        return result.scalars().first()
 
 
 table_crud = CRUDTable(Table)
