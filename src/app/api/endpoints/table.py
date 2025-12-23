29bbae28391a79a3ca738cfa12913b3b7f14ef6c
Marat Garifulun <admin@admin.com>
2025-12-23 23:11:44 +0700

    draft_3

--- src/app/api/endpoints/table.py
+++ src/app/api/endpoints/table.py
@@ -7,7 +7,7 @@ from core.db import get_async_session
 from app.core.user import current_manager_or_admin, current_user
 from crud.table import table_crud
 from models import Table, User
-from schemas.table import TableInfo
+from schemas.table import TableCreate, TableInfo, TableUpdate
 from validators.table import check_cafe_exists
 
 router = APIRouter()
@@ -28,7 +28,7 @@ async def get_table(
 ) -> Table:
     """Получаем стол по ID с учётом прав пользователя."""
     cafe = await check_cafe_exists(cafe_id, session)
-    table = await table_crud.get_by_id_with_permissions(
+    table = await table_crud.get_by_id_id(
         session=session,
         cafe_id=cafe.id,
         id=table_id,
@@ -40,3 +40,57 @@ async def get_table(
             detail=f'Стол {table_id} не найден.',
         )
     return table
+
+
+@router.patch(
+    "/{table_id}",
+    response_model=TableInfo,
+    dependencies=[Depends(current_manager_or_admin)],
+    summary='Обновление информации о столе в кафе по его ID.',
+    description='Обновляет только переданные поля. Для ADMIN и MANAGER.',
+)
+async def update_table(
+    cafe_id: int,
+    table_id: int,
+    update_data: TableUpdate,
+    session: AsyncSession = Depends(get_async_session),
+) -> TableInfo:
+    cafe = await check_cafe_exists(cafe_id, session)
+
+    update_dict = update_data.model_dump(exclude_unset=True)
+
+    if not update_dict:
+        return HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='Данные для обновления не найдены.',
+        )
+
+    return await table_crud.update_by_id_id(
+        session=session,
+        cafe_id=cafe.id,
+        table_id=table_id,
+        update_data=update_dict,
+    )
+
+
+@router.post(
+    "/",
+    response_model=TableInfo,
+    dependencies=[Depends(current_manager_or_admin)],
+    summary="Создаёт новый стол в кафе с указанными параметрами.",
+    description='Доступно только для ADMIN и MANAGER.',
+)
+async def create_table(
+    cafe_id: int,
+    data: TableCreate,
+    session: AsyncSession = Depends(get_async_session),
+) -> TableInfo:
+    cafe = await check_cafe_exists(cafe_id, session)
+
+    new_table = await table_crud.create_table(
+        session=session,
+        cafe_id=cafe.id,
+        description=data.description,
+        seat_number=data.seat_number
+        )
+    return new_table
--- src/app/crud/table.py
+++ src/app/crud/table.py
@@ -1,3 +1,6 @@
+from http import HTTPStatus
+
+from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -8,15 +11,14 @@ from app.models import Table, UserRole
 class CRUDTable(CRUDBase):
     """CRUD для столов."""
 
-    async def get_by_id_with_permissions(
+    async def get_by_id_id(
         self,
         session: AsyncSession,
         cafe_id: int,
         id: int,
         user_role: UserRole
     ) -> Table | None:
-        """
-        Получение стола по ID с учётом прав пользователя:
+        """Получение стола по ID с учётом прав пользователя:
 
         - Для USER: только активные столы (is_active=True)
         - Для ADMIN/MANAGER: любые столы
@@ -31,6 +33,37 @@ class CRUDTable(CRUDBase):
 
         result = await session.execute(query)
         return result.scalars().first()
+
+    async def update_by_id_id(
+        self,
+        session: AsyncSession,
+        cafe_id: int,
+        table_id: int,
+        update_data: dict,
+    ) -> Table | None:
+        """Обновление стола по ID.
+        Вызывающий имеет достаточные права (ADMIN/MANAGER).
+        """
+        query = select(Table).where(
+            Table.cafe_id == cafe_id,
+            Table.id == table_id
+        )
+        result = await session.execute(query)
+        table = result.scalars().first()
+
+        if not table:
+            raise HTTPException(
+                status_code=HTTPStatus.NOT_FOUND,
+                detail=f'Стол {table_id} не найден.',
+            )
+
+        for key, value in update_data.items():
+            if hasattr(table, key):
+                setattr(table, key, value)
+
+        await session.commit()
+        await session.refresh(table)
+        return table
 
 
 table_crud = CRUDTable(Table)
