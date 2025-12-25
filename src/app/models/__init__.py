5db035f2c8547c11e0f58b3188975761956781c1
Sergey Khoetskiy <khoetskiy@gmail.com>
2025-12-25 21:25:11 +0100

    Fix

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,10 +1,10 @@
 from .booking import Booking, TableSlotBooking
 from .cafe import Cafe
-from .enum import BookingStatus
+from .enum import BookingStatus, UserRole
 from .media import Media
 from .slot import Slot
 from .table import Table
-from .user import User, UserRole
+from .user import User
 
 __all__ = [
     'Booking',
