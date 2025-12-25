7c3201b5d4a8d5738ccb8b2ed7bd3b3356141fc5
Sergey Khoetskiy <khoetskiy@gmail.com>
2025-12-25 19:06:33 +0100

    feat: enhance booking schemas with validation and additional fields

--- src/app/schemas/__init__.py
+++ src/app/schemas/__init__.py
@@ -1,5 +1,6 @@
 from .cafe import CafeShortInfo
 from .media import MediaData, MediaInfo
+from .slot import TimeSlotShortInfo
 from .table import (
     TableBase,
     TableCreate,
@@ -7,17 +8,20 @@ from .table import (
     TableShortInfo,
     TableUpdate,
 )
-from .user import UserCreate, UserUpdate
+from .user import UserCreate, UserInfo, UserShortInfo, UserUpdate
 
 __all__ = [
     'CafeShortInfo',
     'MediaData',
     'MediaInfo',
+    'TimeSlotShortInfo',
     'TableBase',
     'TableCreate',
     'TableInfo',
     'TableShortInfo',
     'TableUpdate',
     'UserCreate',
+    'UserInfo',
+    'UserShortInfo',
     'UserUpdate',
 ]
