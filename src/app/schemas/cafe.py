5502308366fb3c495ba799cd33bb11f8e70663d5
Sergey Khoetskiy <khoetskiy@gmail.com>
2026-01-09 14:33:38 +0100

    Delete TODO

--- src/app/schemas/cafe.py
+++ src/app/schemas/cafe.py
@@ -41,7 +41,6 @@ class CafeBase(BaseModel):
         max_length=MAX_LENGTH_CAFE_DESCRIPTION,
         description='Описание кафе',
     )
-    # FIXME: исправить на обязательное и в моделях везде поменять где требуется
     photo_id: UUID | None = Field(
         None,
         description='UUID фотографии кафе',
