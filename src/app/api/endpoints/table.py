b4f40cd3549445612cfbe9d33dbe792eee7c60ce
Marat Garifulun <admin@admin.com>
2026-01-10 19:21:49 +0700

    changes_right

--- src/app/api/endpoints/table.py
+++ src/app/api/endpoints/table.py
@@ -58,7 +58,10 @@ async def get_table(
     cafe = await check_cafe_exists(cafe_id, session)
     logger.debug(
         'Кафе найдено. cafe_id=%d', cafe_id, extra={'cafe_id': cafe_id})
-    show_all = current_active_user.role != UserRole.USER
+    admin = current_active_user.role == UserRole.ADMIN
+    current_cafe_manager = await manager_assigned_to_cafe(
+        session, current_active_user.id, cafe_id)
+    show_all = admin or current_cafe_manager
     table = await table_crud.get_by_cafe_and_id_with_show(
         session=session,
         cafe_id=cafe.id,
@@ -123,7 +126,18 @@ async def update_table(
     )
 
     cafe = await check_cafe_exists(cafe_id, session)
-    if not await manager_assigned_to_cafe(session, current_user.id, cafe_id):
+    if not (
+        await manager_assigned_to_cafe(session, current_user.id, cafe_id)
+        or current_user.role == UserRole.ADMIN
+    ):
+        logger.warning(
+            'Не авторизованный в этом кафе менеджер. cafe_id=%d, user_id=%d',
+            cafe_id, current_user.id,
+            extra={
+                'cafe_id': cafe_id,
+                'user_id': current_user.id,
+            },
+        )
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail='У вас нет прав управлять столами этого кафе.',
@@ -244,7 +258,18 @@ async def create_table(
         },
     )
     await check_cafe_exists(cafe_id, session)
-    if not await manager_assigned_to_cafe(session, current_user.id, cafe_id):
+    if not (
+        await manager_assigned_to_cafe(session, current_user.id, cafe_id)
+        or current_user.role == UserRole.ADMIN
+    ):
+        logger.warning(
+            'Не авторизованный в этом кафе менеджер. cafe_id=%d, user_id=%d',
+            cafe_id, current_user.id,
+            extra={
+                'cafe_id': cafe_id,
+                'user_id': current_user.id,
+            },
+        )
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail='Нельзя создавать столы в чужом кафе.',
@@ -298,8 +323,10 @@ async def list_tables(
     await check_cafe_exists(cafe_id, session)
     logger.debug(
         'Кафе существует. cafe_id=%d', cafe_id, extra={'cafe_id': cafe_id})
-    can_show_all = current_active_user.role in {
-        UserRole.ADMIN, UserRole.MANAGER}
+    admin = current_active_user.role == UserRole.ADMIN
+    current_cafe_manager = await manager_assigned_to_cafe(
+        session, current_active_user.id, cafe_id)
+    can_show_all = admin or current_cafe_manager
     if show_all and can_show_all:
         logger.debug(
             'Администратор/менеджер запрашивает список всех столов cafe_id=%d',
@@ -343,7 +370,6 @@ async def list_tables(
 
 @router.delete(
     '/{table_id}',
-    response_model=TableInfo,
     dependencies=[Depends(current_admin_or_manager)],
     summary='Мягкое удаление стола (деактивация), для ADMIN и MANAGER.',
     description='Деактивирует стол, устанавливая is_active=False.',
@@ -371,7 +397,18 @@ async def delete_table(
     )
 
     cafe = await check_cafe_exists(cafe_id, session)
-    if not await manager_assigned_to_cafe(session, current_user.id, cafe_id):
+    if not (
+        await manager_assigned_to_cafe(session, current_user.id, cafe_id)
+        or current_user.role == UserRole.ADMIN
+    ):
+        logger.warning(
+            'Не авторизованный в этом кафе менеджер. cafe_id=%d, user_id=%d',
+            cafe_id, current_user.id,
+            extra={
+                'cafe_id': cafe_id,
+                'user_id': current_user.id,
+            },
+        )
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail='Нельзя удалять столы в чужом кафе.',
