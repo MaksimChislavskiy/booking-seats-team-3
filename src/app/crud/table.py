cdf1c5966107155d9094e37b8308cc491b42354e
Sergey Khoetskiy <khoetskiy@gmail.com>
2026-01-13 22:25:07 +0100

    Fix linter errors in CRUDTable

--- src/app/crud/table.py
+++ src/app/crud/table.py
@@ -1,15 +1,10 @@
-from datetime import time
-
-from sqlalchemy import and_, select
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from app.crud.base import CRUDBase
 from app.models import Table
 from app.schemas import TableCreate, TableUpdate
 
 
 class CRUDTable(CRUDBase[Table, TableCreate, TableUpdate]):
-    pass
+    """Заглушка."""
 
 
 table_crud = CRUDTable(Table)
